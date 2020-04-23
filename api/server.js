16d5ae7680e7b84c0afecf56ba60082f826b7930
Jensen Koch <jtkoch12@gmail.com>
2020-04-22 19:19:45 -0500

    finished MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
-
+const accountsRouter = require("../accounts/account-router");
 const server = express();
 
 server.use(express.json());
+server.use('/api/accounts', accountsRouter)
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Main route working",
+  })
+})
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
+
 
 module.exports = server;
